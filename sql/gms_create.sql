8c2d1de523efeece1a607bee55aa327746e7dd0e
Stephen Bennett <spb@exherbo.org>
2009-06-10 21:49:13 +0100

    Pluralise table names

--- sql/gms_create.sql
+++ sql/gms_create.sql
@@ -1,22 +1,22 @@
-DROP TABLE IF EXISTS account;
-DROP TABLE IF EXISTS contact;
-DROP TABLE IF EXISTS `group`;
-DROP TABLE IF EXISTS group_contact;
-DROP TABLE IF EXISTS address;
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS user_role;
+DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS groups;
+DROP TABLE IF EXISTS group_contacts;
+DROP TABLE IF EXISTS addresses;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS user_roles;
 
 -- An account in GMS is referred to by this ID. Since
 -- Atheme does not have globally unique account identifiers,
 -- we use a (name, registration time) pair to uniquely identify
 -- a services account.
-CREATE TABLE account (
+CREATE TABLE accounts (
     id              INTEGER PRIMARY KEY AUTO_INCREMENT,
     accountname     VARCHAR(32),
     accountts       TIMESTAMP DEFAULT 0
 );
 
-CREATE TABLE contact (
+CREATE TABLE contacts (
     id              INTEGER PRIMARY KEY AUTO_INCREMENT,
     account_id      INTEGER NOT NULL,
     name            VARCHAR(255),
@@ -24,7 +24,7 @@ CREATE TABLE contact (
 );
 
 
-CREATE TABLE `group` (
+CREATE TABLE groups (
     id              INTEGER PRIMARY KEY AUTO_INCREMENT,
     groupname       VARCHAR(32) NOT NULL,
     grouptype       INTEGER NOT NULL,
@@ -37,13 +37,14 @@ CREATE TABLE `group` (
     approved        TIMESTAMP DEFAULT 0
 );
 
-CREATE TABLE group_contact (
+CREATE TABLE group_contacts (
     group_id        INTEGER NOT NULL,
     contact_id      INTEGER NOT NULL,
-    position        VARCHAR(255)
+    position        VARCHAR(255),
+    PRIMARY KEY (group_id, contact_id)
 );
 
-CREATE TABLE address (
+CREATE TABLE addresses (
     id              INTEGER PRIMARY KEY AUTO_INCREMENT,
     address_one     VARCHAR(255) NOT NULL,
     address_two     VARCHAR(255),
@@ -55,12 +56,12 @@ CREATE TABLE address (
     phone2          VARCHAR(32)
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id              INTEGER PRIMARY KEY AUTO_INCREMENT,
     name            VARCHAR(32)
 );
 
-CREATE TABLE user_role (
+CREATE TABLE user_roles (
     account_id      INTEGER,
     role_id         INTEGER
 );
