4da4715baec51f09919bac8e3ce4466de42a920c
Stephen Bennett <spb@exherbo.org>
2009-06-15 18:48:38 +0100

    Add initial Catalyst-generated web framework

---
+++ lib/GMS/Web/View/TT.pm
@@ -0,0 +1,31 @@
+package GMS::Web::View::TT;
+
+use strict;
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(TEMPLATE_EXTENSION => '.tt');
+
+=head1 NAME
+
+GMS::Web::View::TT - TT View for GMS::Web
+
+=head1 DESCRIPTION
+
+TT View for GMS::Web. 
+
+=head1 SEE ALSO
+
+L<GMS::Web>
+
+=head1 AUTHOR
+
+A clever guy
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
