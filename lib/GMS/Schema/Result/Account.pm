b1c2985bc120213d6bffdfc5451b32b9465af094
Stephen Bennett <spb@exherbo.org>
2009-06-18 22:37:48 +0100

    Update DBIC schema for removal of accounts.accountts column

--- lib/GMS/Schema/Result/Account.pm
+++ lib/GMS/Schema/Result/Account.pm
@@ -5,7 +5,7 @@ use base 'DBIx::Class';
 
 __PACKAGE__->load_components('Core');
 __PACKAGE__->table('accounts');
-__PACKAGE__->add_columns(qw/ id accountname accountts /);
+__PACKAGE__->add_columns(qw/ id accountname /);
 __PACKAGE__->set_primary_key('id');
 
 1;
