b6b6cafa79a76190b2928ba3ec8eea622b94f32c
Stephen Bennett <spb@exherbo.org>
2009-08-19 23:05:21 +0100

    Overload connection() in GMS::Schema to ensure that generated SQL is always quoted.
    This becomes necessary when we use 'group' so frequently, which is of
    course an SQL reserved word.

--- lib/GMS/Schema.pm
+++ lib/GMS/Schema.pm
@@ -5,4 +5,14 @@ use base 'DBIx::Class::Schema';
 
 __PACKAGE__->load_namespaces();
 
+sub connection {
+    my $self = shift;
+    my $rv = $self->next::method( @_ );
+
+    $rv->storage->sql_maker->quote_char("`");
+    $rv->storage->sql_maker->name_sep('.');
+
+    return $rv;
+}
+
 1;
