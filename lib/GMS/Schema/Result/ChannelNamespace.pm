a75ca74156bfac8b92c3b87b2c120d446dcf5345
Stephen Bennett <spb@exherbo.org>
2009-11-06 22:32:54 +0000

    Translate DBIC exceptions from inserting channel namespaces to something more intelligible, where possible

--- lib/GMS/Schema/Result/ChannelNamespace.pm
+++ lib/GMS/Schema/Result/ChannelNamespace.pm
@@ -3,10 +3,27 @@ use strict;
 use warnings;
 use base 'DBIx::Class';
 
+use TryCatch;
+
 __PACKAGE__->load_components('Core');
 __PACKAGE__->table('channel_namespaces');
 __PACKAGE__->add_columns(qw/ group_id namespace /);
 
 __PACKAGE__->belongs_to('group', 'GMS::Schema::Result::Group', 'group_id');
+
+sub insert {
+    my $self = shift;
+    try {
+        return $self->next::method(@_);
+    }
+    catch (DBIx::Class::Exception $e) {
+        if ("$e" =~ /unique_channel_ns/) {
+            die GMS::Exception->new("The channel namespace " . $self->namespace .
+                                    " has already been claimed.");
+        } else {
+            die $e;
+        }
+    }
+}
 
 1;
