eb450c826099602e236f2f25e3bcd12cca2e42f7
Stephen Bennett <spb@exherbo.org>
2009-12-14 19:33:44 +0000

    Make GMS::Config read config from gms_web.conf, instead of duplicating settings. Patch by Khisanth

--- lib/GMS/Config.pm
+++ lib/GMS/Config.pm
@@ -2,16 +2,25 @@ package GMS::Config;
 
 use strict;
 use warnings;
-
+use Config::JFDI;
+use Dir::Self;
 use vars qw($dbstring $dbuser $dbpass
             $atheme_host $atheme_port $service
             $atheme_master_login $atheme_master_pass);
 
-$dbstring = 'dbi:Pg:dbname=gms';
-$dbuser = '';
-$dbpass = '';
-$atheme_host = 'localhost';
-$atheme_port = '8080';
+my $config_loader = Config::JFDI->new(
+	name => "gms_web",
+	path => __DIR__ . "/../..",
+);
+my $config = $config_loader->get;
+
+$dbstring = $config->{"Model::DB"}{connect_info}{dsn};
+$dbuser = $config->{"Model::DB"}{connect_info}{user};
+$dbpass = $config->{"Model::DB"}{connect_info}{password};
+$atheme_host = $config->{"Model::Atheme"}{atheme_host};
+$atheme_port = $config->{"Model::Atheme"}{atheme_port};
 $service = 'GroupServ';
-$atheme_master_login = 'GMS';
-$atheme_master_pass = 'goat';
+$atheme_master_login = $config->{"Model::Atheme"}{master_account};
+$atheme_master_pass = $config->{"Model::Atheme"}{master_password};
+
+1;
