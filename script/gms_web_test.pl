db82109685b48d461838f42fcacde64acb95ef47
Stephen Bennett <spb@exherbo.org>
2010-03-30 23:02:45 +0100

    regenerate catalyst scripts

--- script/gms_web_test.pl
+++ script/gms_web_test.pl
@@ -1,20 +1,7 @@
 #!/usr/bin/env perl
 
-use strict;
-use warnings;
-use Getopt::Long;
-use Pod::Usage;
-use FindBin;
-use lib "$FindBin::Bin/../lib";
-use Catalyst::Test 'GMS::Web';
-
-my $help = 0;
-
-GetOptions( 'help|?' => \$help );
-
-pod2usage(1) if ( $help || !$ARGV[0] );
-
-print request($ARGV[0])->content . "\n";
+use Catalyst::ScriptRunner;
+Catalyst::ScriptRunner->run('GMS::Web', 'Test');
 
 1;
 
@@ -27,7 +14,7 @@ gms_web_test.pl - Catalyst Test
 gms_web_test.pl [options] uri
 
  Options:
-   -help    display this help and exits
+   --help    display this help and exits
 
  Examples:
    gms_web_test.pl http://localhost/some_action
