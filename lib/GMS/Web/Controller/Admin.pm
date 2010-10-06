6682b7c9dbadee5a38e85541374540680e4b15d0
Stephen Bennett <spb@exherbo.org>
2010-10-06 22:36:06 +0100

    Put group approvals/verifications inside a transaction, so that it is treated as a single operation

--- lib/GMS/Web/Controller/Admin.pm
+++ lib/GMS/Web/Controller/Admin.pm
@@ -53,28 +53,30 @@ sub do_approve :Chained('base') :PathPart('approve/submit') :Args(0) {
     my @approve_groups = split / /, $params->{approve_groups};
     my @verify_groups  = split / /, $params->{verify_groups};
 
-    foreach my $group_id (@approve_groups, @verify_groups) {
-        my $group = $group_rs->find({ id => $group_id });
-        my $action = $params->{"action_$group_id"};
-        if ($action eq 'approve') {
-            $c->log->info("Approving group id $group_id (" .
-                $group->groupname . ") by " . $c->user->username . "\n");
-            $group->approve;
-        } elsif ($action eq 'reject') {
-            $c->log->info("Rejecting group id $group_id (" .
-                $group->groupname . ") by " . $c->user->username . "\n");
-            $group->reject;
-        } elsif ($action eq 'verify') {
-            $c->log_info("Verifying group id $group_id (" .
-                $group->groupname . ") by " . $c->user->username . "\n");
-            $group->verify;
-        } elsif ($action eq 'hold') {
-            next;
-        } else {
-            $c->log->error("Got unknown action $action for group id
-                $group_id in Admin::do_approve");
+    $c->model('DB')->schema->txn_do(sub {
+        foreach my $group_id (@approve_groups, @verify_groups) {
+            my $group = $group_rs->find({ id => $group_id });
+            my $action = $params->{"action_$group_id"};
+            if ($action eq 'approve') {
+                $c->log->info("Approving group id $group_id (" .
+                    $group->groupname . ") by " . $c->user->username . "\n");
+                $group->approve;
+            } elsif ($action eq 'reject') {
+                $c->log->info("Rejecting group id $group_id (" .
+                    $group->groupname . ") by " . $c->user->username . "\n");
+                $group->reject;
+            } elsif ($action eq 'verify') {
+                $c->log_info("Verifying group id $group_id (" .
+                    $group->groupname . ") by " . $c->user->username . "\n");
+                $group->verify;
+            } elsif ($action eq 'hold') {
+                next;
+            } else {
+                $c->log->error("Got unknown action $action for group id
+                    $group_id in Admin::do_approve");
+            }
         }
-    }
+    });
 
     $c->response->redirect($c->uri_for('approve'));
 }
