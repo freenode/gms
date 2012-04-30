d76a1843e411518b5d9591fa6eeb3b5706fe74e9
Errietta Kostala <errietta@hotmail.com>
2012-04-30 20:02:29 +0300

    Add tooltips in the group registration form like in the grf-f form.

--- root/static/main.js
+++ root/static/main.js
@@ -19,3 +19,44 @@ function hide(elem_id) {
         link.setAttribute("onclick", "show(" + elem_id + ")");
     }
 }
+
+function tooltip (elem, id) {
+    untooltip();
+    var div = document.createElement ("div");
+    div.className = "tooltip";
+    div.id = "tooltip";
+
+    var parent = elem.parentNode;
+
+    /* get the position of the table row the tooltip image is in. */
+    var x = parent.getBoundingClientRect().left;
+    var y = parent.getBoundingClientRect().top;
+
+    /* how much the user has scrolled, annoyingly different for some browsers */
+    var scrollX = (window.scrollX?window.scrollX:document.documentElement.scrollLeft);
+    var scrollY = (window.scrollY?window.scrollY:document.documentElement.scrollTop);
+
+    /* position the element */
+    div.style.position = "absolute";
+    div.style.left = (x + scrollX) + 'px';
+    div.style.top = (y + 25 + scrollY) + 'px';
+
+    var windowWidth = document.documentElement.offsetWidth;
+
+    div.style.maxWidth = (windowWidth - x - 50) + 'px';
+
+    /* the texts are located in hidden elements to make code a bit clearer
+       than it would be if we passed them to the function */
+    var text = document.getElementById('tooltipText' + id).innerHTML;
+
+    div.innerHTML = text;
+    document.body.appendChild (div);
+}
+
+function untooltip () {
+    var elem;
+
+    if ( ( elem = document.getElementById ("tooltip") ) ) {
+        elem.parentNode.removeChild(elem);
+    }
+}
