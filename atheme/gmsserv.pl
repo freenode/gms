0717e9a5fde2ae70b600f66ab7a7b2643b5fce51
Errietta Kostala <errietta@hotmail.com>
2013-05-21 21:53:44 +0100

    Add new features to gmsserv.pl, plus make it look up accounts via UID

--- atheme/gmsserv.pl
+++ atheme/gmsserv.pl
@@ -47,6 +47,22 @@ $Services{GMSServ}->bind_command(
     access => "special:gms"
 );
 
+$Services{GMSServ}->bind_command(
+    name => "ACCOUNTNAME",
+    desc => "Returns a user's account name from their UID",
+    help_path => "gmssrv/accountname",
+    handler => \&gms_accountname,
+    access => "special:gms"
+);
+
+$Services{GMSServ}->bind_command(
+    name => "CHANEXISTS",
+    desc => "Returns 1 if a channel is registered, -1 if not.",
+    help_path => "gmsserv/chanexists",
+    handler => \&gms_chanexists,
+    access => "special:gms"
+);
+
 sub gms_uid {
     my ($source, @parv) = @_;
 
@@ -65,23 +81,23 @@ sub gms_uid {
 sub gms_drop {
     my ($source, @parv) = @_;
 
-    my ($channel, $nick) = @parv;
+    my ($channel, $uid) = @parv;
 
     my $creg = $Atheme::ChannelRegistrations{$channel};
 
-    my $acc = $Atheme::Accounts{$nick};
+    my $acc = $Atheme::Accounts{'?' . $uid};
 
     if (!$creg) {
         $source->fail (Atheme::Fault::nosuch_target(), "The channel $channel is not registered");
     } elsif (!$acc) {
-        $source->fail (Atheme::Fault::nosuch_target(), "The nickname $nick is not registered");
+        $source->fail (Atheme::Fault::nosuch_target(), "The account with uid $uid is not registered");
     } else {
         $creg->drop;
 
         $source->success ("The channel " . $channel . " has been dropped");
 
-        Atheme::Log::command(__PACKAGE__, $source, Atheme::Log::admin | Atheme::Log::register, "The channel $channel has been dropped through GMS by $nick");
-        Atheme::wallops ("The channel $channel has been dropped through GMS by $nick");
+        Atheme::Log::command(__PACKAGE__, $source, Atheme::Log::admin | Atheme::Log::register, "The channel $channel has been dropped through GMS by " . $acc->name);
+        Atheme::wallops ("The channel $channel has been dropped through GMS by " . $acc->name);
     }
 
 }
@@ -89,52 +105,52 @@ sub gms_drop {
 sub gms_transfer {
     my ($source, @parv) = @_;
 
-    my ($channel, $new_founder, $command_caller) = @parv;
+    my ($channel, $new_founder, $requestor) = @parv;
 
     my $creg = $Atheme::ChannelRegistrations{$channel};
 
-    my $nf_acc = $Atheme::Accounts{$new_founder};
-    my $caller_acc = $Atheme::Accounts{$command_caller};
+    my $nf_acc = $Atheme::Accounts{'?' . $new_founder};
+    my $requestor_acc = $Atheme::Accounts{'?' . $requestor};
 
     if (!$creg) {
         $source->fail (Atheme::Fault::nosuch_target(), "The channel $channel is not registered");
     } elsif (!$nf_acc) {
-        $source->fail (Atheme::Fault::nosuch_target(), "The nickname $new_founder is not registered");
-    } elsif (!$caller_acc) {
-        $source->fail (Atheme::Fault::nosuch_target(), "The nickname $command_caller is not registered");
+        $source->fail (Atheme::Fault::nosuch_target(), "The account with uid $new_founder is not registered");
+    } elsif (!$requestor_acc) {
+        $source->fail (Atheme::Fault::nosuch_target(), "The account with uid $requestor is not registered");
     } else {
         $creg->transfer ($source, $nf_acc);
 
-        $source->success ("The channel " . $channel . " has been transferred to $new_founder");
+        $source->success ("The channel " . $channel . " has been transferred to " . $nf_acc->name);
 
-        Atheme::Log::command(__PACKAGE__, $source, Atheme::Log::admin | Atheme::Log::register, "The channel $channel has been transferred to $new_founder through GMS by $command_caller");
-        Atheme::wallops("The channel $channel has been transferred to $new_founder through GMS by $command_caller");
+        Atheme::Log::command(__PACKAGE__, $source, Atheme::Log::admin | Atheme::Log::register, "The channel $channel has been transferred to " . $nf_acc->name . " through GMS by " . $requestor_acc->name);
+        Atheme::wallops("The channel $channel has been transferred to " . $nf_acc->name . " through GMS by " . $requestor_acc->name);
     }
 }
 
 sub gms_cloak {
     my ($source, @argv) = @_;
 
-    my ($nick, $cloak) = @argv;
+    my ($uid, $cloak) = @argv;
 
-    my $acc = $Atheme::Accounts{$nick};
+    my $acc = $Atheme::Accounts{'?' . $uid};
 
     if (!$acc) {
         $source->fail (Atheme::Fault::nosuch_target(), "No such account");
+    } else {
+        $acc->vhost ($cloak);
+
+        $source->success ($acc->name . " has successfully been granted the cloak $cloak");
+        Atheme::Log::command(__PACKAGE__, $source, Atheme::Log::admin, "VHOST:ASSIGN: $cloak to " . $acc->name);
     }
-
-    $acc->vhost ($cloak);
-
-    $source->success ($nick . " has successfully been granted the cloak $cloak");
-    Atheme::Log::command(__PACKAGE__, $source, Atheme::Log::admin, "VHOST:ASSIGN: $cloak to $nick");
 }
 
 sub gms_metadata {
     my ($source, @argv) = @_;
 
-    my ($nick, $name) = @argv;
+    my ($uid, $name) = @argv;
 
-    my $acc = $Atheme::Accounts{$nick};
+    my $acc = $Atheme::Accounts{'?' . $uid};
 
     if (!$acc) {
         $source->fail (Atheme::Fault::nosuch_target(), "No such account");
@@ -151,3 +167,29 @@ sub gms_metadata {
 
 }
 
+sub gms_accountname {
+    my ($source, @argv) = @_;
+
+    my $uid = shift @argv;
+
+    my $acc = $Atheme::Accounts{'?' . $uid}; # '?' And a UID will find an account name via UID per Atheme code.
+
+    if (!$acc) {
+        $source->fail (Atheme::Fault::nosuch_target(), "No such account");
+    } else {
+        $source->success ($acc->name);
+    }
+}
+
+sub gms_chanexists {
+    my ($source, @parv) = @_;
+    my $channel = shift @parv;
+
+    my $creg = $Atheme::ChannelRegistrations{$channel};
+
+    if ($creg) {
+        $source->success (1);
+    } else {
+        $source->success (-1);
+    }
+}
