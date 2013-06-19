eb314daa803c044427c013dfe8cc2b507fb6b12a
Errietta Kostala <errietta@hotmail.com>
2013-06-19 12:43:41 +0100

    Hide the gab/listing stuff if 'no' is selected.

---
+++ root/static/group_new.js
@@ -0,0 +1,60 @@
+function groupNewPage() {
+    var join_gab_y = document.getElementById('join_gab_y');
+    var join_gab_n = document.getElementById('join_gab_n');
+    var include_listing_y = document.getElementById('include_listing_y');
+    var include_listing_n = document.getElementById('include_listing_n');
+
+    join_gab_y.onclick = function() {
+        toggleGAB();
+    }
+    join_gab_n.onclick = function() {
+        toggleGAB();
+    }
+    include_listing_y.onclick = function() {
+        toggleList();
+    }
+    include_listing_n.onclick = function() {
+        toggleList();
+    }
+
+    toggleGAB();
+    toggleList();
+}
+
+function toggleGAB() {
+    var join_gab_y = document.getElementById('join_gab_y');
+    var join_gab_n = document.getElementById('join_gab_n');
+
+    if ( join_gab_y.checked ) {
+        showGAB(true);
+    } else if ( join_gab_n.checked ) {
+        showGAB(false);
+    }
+}
+
+function showGAB(show) {
+    document.getElementById('gab').style.display = ( show ? '' : 'none' );
+}
+
+function toggleList() {
+    var include_listing_y = document.getElementById('include_listing_y');
+    var include_listing_n = document.getElementById('include_listing_n');
+
+    if ( include_listing_y.checked ) {
+        showList(true);
+    } else if ( include_listing_n.checked ) {
+        showList(false);
+    }
+}
+
+function showList(show) {
+    document.getElementById('listing').style.display = ( show ? '' : 'none' );
+}
+
+addEventHandler ( window, 'load', function() {
+    setTimeout (
+            function() {
+                groupNewPage();
+            },
+    1);
+});
