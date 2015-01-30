4158fcd6d4e84603025a735c9c045c220fc3854a
Maxwell Dreytser <admin@mdtech.us>
2015-01-30 15:02:22 -0500

    More readable deploy.pl
    Added some actual commenting to deploy.pl.
    Also made things more readable.

--- bin/deploy.pl
+++ bin/deploy.pl
@@ -21,7 +21,7 @@ BEGIN {
     has _dh => (is => 'lazy');
 
     sub _build__dh {
-        my ($self) = @_;
+
         DBIx::Class::DeploymentHandler->new({
                 schema => GMS::Schema->do_connect,
                 script_directory => 'share/ddl',
@@ -31,7 +31,9 @@ BEGIN {
     }
 
     sub cmd_write_ddl {
-        my ($self) = @_;
+        # Get self.
+        my $self = shift;
+
         $self->_dh->prepare_install;
         my $v = $self->_dh->schema_version;
         if ($v > 1) {
@@ -42,20 +44,52 @@ BEGIN {
         }
     }
 
-    sub cmd_install {  my$self=shift;$self->_dh->install; $self->_dh->schema->install_defaults; }
+    # Do a first-time database install.
+    sub cmd_install {
+        # Get self.
+        my $self = shift;
 
-    sub cmd_upgrade { shift->_dh->upgrade }
+        # Install.
+        $self->_dh->install;
 
+        # Setup Schema.
+        $self->_dh->schema->install_defaults;
+    }
+
+    # Do an upgrade from a previous version.
+    sub cmd_upgrade {
+        # Get self.
+        my $self = shift;
+
+        # Do upgrade.
+        $self->_dh->upgrade;
+    }
+
+    # This is the main sub that is run at the start.
     sub run {
         my ($self) = @_;
+
+        # Get the command and any extra parameters supplied
+        # from the command line.
         my ($cmd, @what) = @ARGV;
+
+        # Check to see that a command was passed. If not, exit.
         die "Must supply a command\n" unless $cmd;
+
+        # TBD: I suggest that a -h/--help option be added,
+        # That explains what each command does.
+
+        # Check to see if any extra, bogus parameters were passed.
+        # TBD: Should we really exit?
         die "Extra argv detected - command only please\n" if @what;
+
+        # Check if the supplied command is valid.
         die "No such command ${cmd}\n" unless $self->can("cmd_${cmd}");
+
+        # Execute the command.
         $self->${\"cmd_${cmd}"};
     }
 
 } # end of BEGIN block
 
 GMS::Schema::Script->new->run;
-
